576dd561bdc1d01a0ad3eca1e29ca4037a3fd8b3
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-15 16:36:10 -0500

    deployment errors

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -14,10 +14,10 @@ class ErrorBoundary extends Component {
 
 	render() {
 		if(this.state.hasError) {
-			return <h1>Oops. That's not good!</h1>;
+			return <h1>Oops. That's not good!</h1>
 		}
 		return this.props.children;
 	}
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
